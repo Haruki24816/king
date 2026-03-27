77db0a7bd828c1b85b88dd9d728a6116940c3f24
Haruki <haruki24816@gmail.com>
2026-03-27 23:34:01 +0900

    画像軽量化

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="fill-height" max-width="300">
     <div class="w-100">
-      <v-img class="mb-8" rounded="xl" src="../assets/logo.png" />
+      <v-img class="mb-8" rounded="xl" src="../assets/logo.jpg" />
       <slot></slot>
     </div>
   </v-container>
