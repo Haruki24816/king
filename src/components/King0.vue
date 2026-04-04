d8e4aa2101ddf81a5ed3ea25563c6fd841db3c3e
Haruki <haruki24816@gmail.com>
2026-04-05 01:45:20 +0900

    ui

--- src/components/King0.vue
+++ src/components/King0.vue
@@ -1,24 +1,55 @@
 <template>
-  <div>プレイヤー一覧（リンクを共有して招待してください）</div>
-  <div v-for="userId in room.getUserIds()">
-    {{ room.data.users[userId].userName }}
-    {{ room.data.users[userId].order }}
-    <v-btn @click="system.operateStore('room', 'upUserOrder', userId)">↑</v-btn>
-    <v-btn @click="system.operateStore('room', 'downUserOrder', userId)">↓</v-btn>
-  </div>
-  <v-btn @click="system.operateStore('room', 'updateUserOrder', room.getShuffledUserIds())">
-    順番シャッフル
-  </v-btn>
-  <v-btn @click="start">
-    ゲーム開始
-  </v-btn>
+  <v-card class="mb-4">
+    <v-card-text>
+      <div class="text-center my-4">
+        プレイヤーを招待してください
+      </div>
+      <v-text-field label="招待URL" hint="コピーしました" :model-value="url" readonly @click="share"></v-text-field>
+    </v-card-text>
+  </v-card>
+  <template v-if="1 < room.getUserCount()">
+    <v-card class="mb-4">
+      <v-card-text>
+        <div class="text-center mt-4">
+          順番を決めてください
+        </div>
+        <v-list>
+          <template v-for="userId in room.getUserIds()">
+            <v-list-item>
+              {{ room.data.users[userId].order + 1 }}. {{ room.data.users[userId].userName }}
+              <template v-slot:append>
+                <v-btn class="position-absolute swap-button" icon="mdi-swap-vertical" density="comfortable"
+                  v-if="room.data.users[userId].order + 1 != room.getUserCount()"
+                  @click="system.operateStore('room', 'downUserOrder', userId)"></v-btn>
+              </template>
+            </v-list-item>
+            <v-divider v-if="room.data.users[userId].order + 1 != room.getUserCount()"></v-divider>
+          </template>
+        </v-list>
+      </v-card-text>
+    </v-card>
+    <v-btn class="mb-4" block @click="system.operateStore('room', 'updateUserOrder', room.getShuffledUserIds())">
+      順番シャッフル
+    </v-btn>
+    <v-btn block @click="start">
+      ゲーム開始
+    </v-btn>
+  </template>
 </template>
 
 <script setup>
+import { computed } from "vue"
 import { system } from "../system"
 
-const king = system.stores.king
 const room = system.stores.room
+const king = system.stores.king
+
+const url = computed(() => window.location.href)
+
+function share() {
+  navigator.clipboard.writeText(url.value)
+  navigator.share({ url: url.value })
+}
 
 function start() {
   system.operateStore(
@@ -34,3 +65,10 @@ function start() {
   )
 }
 </script>
+
+<style scoped>
+.swap-button {
+  top: 30px;
+  z-index: 1;
+}
+</style>
--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container class="fill-height d-flex flex-column" max-width="600">
-    <v-toolbar class="mb-4" density="compact" rounded>
-      <v-toolbar-title :text="system.stores.room.data.roomName"></v-toolbar-title>
+  <v-container max-width="400">
+    <v-toolbar class="mb-4" density="compact" color="white" elevation=1 rounded>
+      <v-toolbar-title :text="room.data.roomName"></v-toolbar-title>
       <v-menu location="bottom end">
         <template v-slot:activator="{ props }">
           <v-btn icon="mdi-dots-vertical" v-bind="props"></v-btn>
@@ -13,10 +13,15 @@
         </v-list>
       </v-menu>
     </v-toolbar>
-    <King />
+    <King0 v-if="system.stores.king.data.stat == 0" />
+    <King1 v-if="system.stores.king.data.stat == 1" />
+    <King2 v-if="system.stores.king.data.stat == 2" />
+    <King3 v-if="system.stores.king.data.stat == 3" />
   </v-container>
 </template>
 
 <script setup>
 import { system } from "../system"
+
+const room = system.stores.room
 </script>
